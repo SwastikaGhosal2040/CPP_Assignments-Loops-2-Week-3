2c7e0088df4d4613843dadc6f6639e85b98bb3f8
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-15 01:12:40 +0530

    Add files via upload

---
+++ Ques8/8_firstNfibonacci.cpp
@@ -0,0 +1,23 @@
+/*Print first ‘n’ fibonacci numbers.
+Sample Input : 10
+Output :
+
+1 1 2 3 5 8 13 21 34 55
+
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int a=1,b=1,sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<a<<" ";
+        sum=a+b;
+        a=b;
+        b=sum;
+    }
+}
